bdb9aa45abd3541131c54e053d8c3cc114c8e1df
spoorjith1 <spoorjith1@gmail.com>
2026-06-15 12:26:49 +0530

    resunme added

--- frontend-react/src/pages/Resume.jsx
+++ frontend-react/src/pages/Resume.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 function Resume() {
   return (
-    <div>
-      Resume
+    <div className='resume-page'>
+      <h2 className='resume-heading'>Resume</h2>
+      <div className='resume-container'>
+        <div className='pdf-options'>
+          <div className='options-container'>
+            <a href='/spoorjith_resume1.pdf' target='_blank' rel='noopener noreferrer' className='btn resume-download-btn'>View Resume PDF</a>
+            <a href='/spoorjith_resume1.pdf' download='spoorjith_resume.pdf' className='btn resume-download-btn'>Download Resume PDF</a>
+          </div>
+        </div>
+        <div>
+          <img src='/resume1.png' alt='resume-preview' className='resume-screenshot' />
+        </div>
+      </div>
     </div>
   )
 }
