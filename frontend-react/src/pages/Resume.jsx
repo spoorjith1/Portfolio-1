59599588659fd0fc50097c5cf0f4b73b3a308ed6
spoorjith1 <spoorjith1@gmail.com>
2026-07-13 09:35:17 +0530

    updated resume

--- frontend-react/src/pages/Resume.jsx
+++ frontend-react/src/pages/Resume.jsx
@@ -7,8 +7,8 @@ function Resume() {
       <div className='resume-container'>
         <div className='pdf-options'>
           <div className='options-container'>
-            <a href='/spoorjith_resume3.pdf' target='_blank' rel='noopener noreferrer' className='btn resume-download-btn'>View Resume PDF</a>
-            <a href='/spoorjith_resume3.pdf' download='spoorjith_resume.pdf' className='btn resume-download-btn'>Download Resume PDF</a>
+            <a href='/spoorjith_resume4.pdf' target='_blank' rel='noopener noreferrer' className='btn resume-download-btn'>View Resume PDF</a>
+            <a href='/spoorjith_resume4.pdf' download='spoorjith_resume.pdf' className='btn resume-download-btn'>Download Resume PDF</a>
           </div>
         </div>
         <div>
